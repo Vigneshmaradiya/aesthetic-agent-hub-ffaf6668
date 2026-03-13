a0781e6f16a2038a82a1bdb39c136e3dc6100097
Vignesh Maradiya <maradiyavignesh2004@gmail.com>
2026-03-13 16:46:41 +0530

    Delete src/app directory

--- src/app/admin/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllPrompts } from "@/lib/prompts/prompt-registry";
-import PromptEditor from "./PromptEditor";
-
-export const dynamic = "force-dynamic";
-
-export default function AdminPage() {
-  const prompts = getAllPrompts();
-  return <PromptEditor initialPrompts={prompts} />;
-}
